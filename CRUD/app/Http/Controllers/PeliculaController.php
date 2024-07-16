cbe9dd71bc2e80a963ac293322e729e611d50d4e
Anderson <beltrananderson283@gmail.com>
2024-07-16 15:43:23 -0500

    Controlador listo

---
+++ CRUD/app/Http/Controllers/PeliculaController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelicula;
+use Illuminate\Http\Request;
+
+class PeliculaController extends Controller
+{
+    // Metodo para mostrar todas las peliculas.
+    public function index(){
+        $peliculas = Pelicula::orderBy('fecha_publicacion', 'desc');
+        return $peliculas;
+    }
+
+    // Metodo para mostrar el formulario de creación de peliculas.
+    public function create(){
+        return view('peliculas.create');
+    }
+
+    // Metodo para registrar una pelicula.
+    public function store(Request $request){
+        $pelicula = new Pelicula();
+        $pelicula->titulo = $request->titulo;
+        $pelicula->fecha_publicacion = $request->fecha_publicacion;
+        $pelicula->save();
+        return $pelicula;
+    }
+}
